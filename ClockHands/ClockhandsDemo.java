c1c0a1a3f0052da3a8bf843ae97c2ae0acdf4e45
Pat-Earning <33377292+Pat-Earning@users.noreply.github.com>
2018-03-12 11:37:58 +0100

    Create ClockhandsDemo.java

---
+++ ClockHands/ClockhandsDemo.java
@@ -0,0 +1,15 @@
+package ClockHands;
+
+public Class ClockhandsDemo() {
+
+  public static main(){
+    Calendar time = "10:22";
+    Float hourHand;
+    Float minuteHand;
+    
+    hourHand = ClockHands.HourHandAngle(time);
+    minuteHand = Clockahnds.MinuteHandAngle(time);
+    
+  }
+
+}
